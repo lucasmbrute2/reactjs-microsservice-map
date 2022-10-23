63f66eecea46bf52b4dfb3603b594820212076ab
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2022-10-23 11:35:41 -0300

    feat: Importing snack provider into entire app

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import { Mapping } from "./components/Mapping";
 import { ThemeProvider } from "@mui/material";
 import { theme } from "./components/theme";
 import CssBaseline from "@mui/material/CssBaseline";
+import { SnackbarProvider } from "notistack";
 
 function App() {
     return (
         <ThemeProvider theme={theme}>
-            <CssBaseline />
-            <Mapping />
+            <SnackbarProvider maxSnack={3}>
+                <CssBaseline />
+                <Mapping />
+            </SnackbarProvider>
         </ThemeProvider>
     );
 }
