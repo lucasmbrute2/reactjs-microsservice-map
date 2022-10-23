30ee4807c618e00a324c4bb132effadd117a7689
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2022-10-23 11:20:57 -0300

    feat: implemented color pallete

--- src/components/Mapping.tsx
+++ src/components/Mapping.tsx
@@ -1,19 +1,38 @@
 import { Button, Grid, MenuItem, Select } from "@mui/material";
 import { Loader } from "google-maps";
-import { useState, useEffect, FormEvent, useCallback, useRef } from "react";
+import {
+    useState,
+    useEffect,
+    FormEvent,
+    useCallback,
+    useRef,
+    FunctionComponent,
+} from "react";
 import { getCurrentPosition } from "../util/geolocation";
-import { makeCarIcon, makeMarkerIcon } from "../util/map";
+import { makeCarIcon, makeMarkerIcon, Map } from "../util/map";
 import { Route } from "../util/models";
-
-type Props = {};
+import { sample, shuffle } from "lodash";
 
 const API_URL = process.env.REACT_APP_API_URL;
 const googleMapsLoader = new Loader(process.env.REACT_APP_GOOGLE_API_KEY);
 
-export const Mapping = (props: Props) => {
+const colors = [
+    "#b71c1c",
+    "#4a148c",
+    "#2e7d32",
+    "#e65100",
+    "#2962ff",
+    "#c2185b",
+    "#FFCD00",
+    "#3e2723",
+    "#03a9f4",
+    "#827717",
+];
+
+export const Mapping: FunctionComponent = () => {
     const [routes, setRoutes] = useState<Route[]>([]);
     const [routeIdSelected, setRouteIdSelected] = useState<string>("");
-    const mapRef = useRef<google.maps.Map>();
+    const mapRef = useRef<Map>();
 
     useEffect(() => {
         async function getRoutes() {
@@ -32,7 +51,7 @@ export const Mapping = (props: Props) => {
             ]);
 
             const divMap = document.getElementById("map") as HTMLElement;
-            mapRef.current = new google.maps.Map(divMap, {
+            mapRef.current = new Map(divMap, {
                 zoom: 15,
                 center: position,
             });
@@ -43,18 +62,16 @@ export const Mapping = (props: Props) => {
         (event: FormEvent) => {
             event.preventDefault();
             const route = routes.find((r) => r._id === routeIdSelected);
-            new google.maps.Marker({
-                position: route?.startPosition,
-                map: mapRef.current,
-                icon: {
-                    path: makeCarIcon(),
+            const color = sample(shuffle(colors)) as string;
+
+            mapRef.current?.addRoute(routeIdSelected, {
+                currentMarketOptions: {
+                    position: route?.startPosition,
+                    icon: makeCarIcon(color),
                 },
-            });
-            new google.maps.Marker({
-                position: route?.endPosition,
-                map: mapRef.current,
-                icon: {
-                    path: makeMarkerIcon(),
+                endMarkerOptions: {
+                    position: route?.endPosition,
+                    icon: makeMarkerIcon(color),
                 },
             });
         },
